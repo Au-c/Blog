a72426669732fbe96d953f090d82870ded5abb06
Au-c <firefirefold@gmail.com>
2020-11-07 23:16:56 +0800

    test: 测试忽略文件

--- docs/.vuepress/config/secret.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  // clientId: "219b065fe1ebdc9a72ad",
-  // clientSecret: "3ca8ecea176b4b038ec7062ae1dadb19c1487342",
-  appId: "aahptw02ExhF0IfKlpclDG8o-9Nh9j0Va",
-  appKey: "BBevescVsdVkxVs7b9De0xnn",
-  ga: "UA-181762966-1", // UA-00000000-0
-};
